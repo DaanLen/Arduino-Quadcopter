96924a96aadcde4d21276e2d40be65388b6c052c
Daan <daan.lenssen@gmail.com>
2021-11-20 14:22:32 +0100

    Initial file creation

---
+++ Quadcopter/Quadcopter.ino
@@ -0,0 +1,16 @@
+//Arduino layout with connections
+//Radio       MISO MOSI SCLK 2pin   pins x+x+x+x+x  5V+GND    CSN -> x  CE -> x
+
+
+//Define pins
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
