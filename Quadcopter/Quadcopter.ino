60ce1bea110670abb1b8a134f7e98249741c5d00
Daan <daan.lenssen@gmail.com>
2021-11-21 13:28:53 +0100

    Add Receiver
    added basic receiver to print received message to serial.

--- Quadcopter/Quadcopter.ino
+++ Quadcopter/Quadcopter.ino
@@ -2,15 +2,33 @@
 //Radio       MISO MOSI SCLK 2pin   pins x+x+x+x+x  5V+GND    CSN -> x  CE -> x
 
 
+#include <SPI.h>
+#include <RF24.h>
+
+RF24 radio(7,10); //CE, CSN
+const byte address = "000001";
 //Define pins
 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+
+  //Setup radio
+  radio.begin();
+  radio.openReadingPipe(0,address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  //Read Radio
+  if (radio.available()) {
+    char text[32] = "";
+    radio.read(&text, sizeof(text));
+    Serial.println(text);
+  }
 
 }
