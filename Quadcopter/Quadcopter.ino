15cda7d1cb21fe5ca4d9b6a5767b0f99f6e2f0a1
Daan <daan.lenssen@gmail.com>
2021-12-09 21:39:55 +0100

    Update Receiver
    Now receives Joystick array

--- Quadcopter/Quadcopter.ino
+++ Quadcopter/Quadcopter.ino
@@ -1,34 +1,30 @@
 //Arduino layout with connections
-//Radio       MISO MOSI SCLK 2pin   pins x+x+x+x+x  5V+GND    CSN -> x  CE -> x
-
+//Radio       MISO MOSI SCLK 2pin   pins 12+11+13+7+8  5V+GND    CSN -> 7  CE -> 8
 
 #include <SPI.h>
 #include <RF24.h>
 
-RF24 radio(7,10); //CE, CSN
-const byte address = "00001";
-//Define pins
 
+RF24 radio(8,7); //CE, CSN WAAROM????
+
+const byte address[6] = "00001";
+int Joysticks[] = {0, 0, 0, 0};
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-
-  //Setup radio
   radio.begin();
-  radio.openReadingPipe(0,address);
+  radio.openReadingPipe(0, address);
   radio.setPALevel(RF24_PA_MIN);
   radio.startListening();
+
+  //pinMode(2,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-  //Read Radio
-  if (radio.available()) {
-    char text[32] = "";
-    radio.read(&text, sizeof(text));
-    Serial.println(text);
+  if (radio.available()){
+    radio.read(&Joysticks, sizeof(Joysticks));
+    Serial.println(Joysticks[1]);
   }
-
 }
